8aa19fa5d5ae8e875d533b549596d8d2a334b995
Willian Dutra <contatowilliandutra@gmail.com>
2023-07-10 09:48:28 -0300

    Projeto Trybank - sem arquivos da Trybe

---
+++ src/trybank/Trybank.cs
@@ -0,0 +1,194 @@
+﻿namespace trybank;
+
+public class Trybank
+{
+	public bool Logged;
+	public int loggedUser;
+
+	//0 -> Número da conta
+	//1 -> Agência
+	//2 -> Senha
+	//3 -> Saldo
+	public int[,] Bank;
+	public int registeredAccounts;
+	private int maxAccounts = 50;
+	public Trybank()
+	{
+		loggedUser = -99;
+		registeredAccounts = 0;
+		Logged = false;
+		Bank = new int[maxAccounts, 4];
+	}
+
+	// 1. Construa a funcionalidade de cadastrar novas contas
+	public void RegisterAccount(int number, int agency, int pass)
+	{
+		try
+		{
+			for (int i = 0; i < registeredAccounts; i++)
+			{
+				if (Bank[i, 0] == number && Bank[i, 1] == agency)
+				{
+					throw new ArgumentException("A conta já está sendo usada!");
+				}
+			}
+
+			Bank[registeredAccounts, 0] = number;
+			Bank[registeredAccounts, 1] = agency;
+			Bank[registeredAccounts, 2] = pass;
+			Bank[registeredAccounts, 3] = 0;
+			registeredAccounts++;
+			return;
+
+		}
+		catch (ArgumentException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	// 2. Construa a funcionalidade de fazer Login
+	public void Login(int number, int agency, int pass)
+	{
+		try
+		{
+			if (Logged == true)
+				throw new AccessViolationException("Usuário já está logado");
+
+			for (int i = 0; i < registeredAccounts; i++)
+			{
+				if (Bank[i, 0] == number && Bank[i, 1] == agency)
+				{
+					if (Bank[i, 2] == pass)
+					{
+						Logged = true;
+						loggedUser = i;
+						return;
+					}
+					else
+					{
+						throw new ArgumentException("Senha incorreta");
+					}
+				}
+			}
+
+			throw new ArgumentException("Agência + Conta não encontrada");
+		}
+		catch (AccessViolationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+		catch (ArgumentException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	// 3. Construa a funcionalidade de fazer Logout
+	public void Logout()
+	{
+		try
+		{
+			if (Logged == false)
+				throw new AccessViolationException("Usuário não está logado");
+
+			Logged = false;
+			loggedUser = -99;
+		}
+		catch (AccessViolationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	// 4. Construa a funcionalidade de checar o saldo
+	public int CheckBalance()
+	{
+		try
+		{
+			if (Logged == false)
+				throw new AccessViolationException("Usuário não está logado");
+
+			return Bank[loggedUser, 3];
+		}
+		catch (AccessViolationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	// 5. Construa a funcionalidade de depositar dinheiro
+	public void Deposit(int value)
+	{
+		try
+		{
+			if (Logged == false)
+				throw new AccessViolationException("Usuário não está logado");
+
+			Bank[loggedUser, 3] += value;
+		}
+		catch (AccessViolationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	// 6. Construa a funcionalidade de sacar dinheiro
+	public void Withdraw(int value)
+	{
+		try
+		{
+			if (Logged == false)
+				throw new AccessViolationException("Usuário não está logado");
+			if (value > Bank[loggedUser, 3])
+				throw new InvalidOperationException("Saldo insuficiente");
+			else
+				Bank[loggedUser, 3] -= value;
+		}
+		catch (AccessViolationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+		catch (InvalidOperationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	// 7. Construa a funcionalidade de transferir dinheiro entre contas
+	public void Transfer(int destinationNumber, int destinationAgency, int value)
+	{
+		try
+		{
+			if (Logged == false)
+				throw new AccessViolationException("Usuário não está logado");
+			if (value > Bank[loggedUser, 3])
+				throw new InvalidOperationException("Saldo insuficiente");
+
+			for (int i = 0; i < registeredAccounts; i++)
+			{
+				if (Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+				{
+					Bank[loggedUser, 3] -= value;
+					Bank[i, 3] += value;
+					return;
+				}
+			}
+		}
+		catch (AccessViolationException ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+
+}
